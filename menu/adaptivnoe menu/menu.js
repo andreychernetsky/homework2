7e1ff028b70762e12fb7e28929477cb3135cc226
andreychernetsky <rodomir3119841985@gmail.com>
2018-05-20 19:13:36 +0300

    first commit

---
+++ menu/adaptivnoe menu/menu.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+
+document.querySelector('#menu').onmouseover = menuShow;
+document.querySelector('#menu').onmouseover = menuHide;
+
+document.onkeydown = function(event){
+	// console.log(event);
+	if(event.code == 'keyM') menuShow();
+	if(event.code == 'Escape') menuHide();
+}
+
+	function menuShow(){
+		document.querySelector('#menu').style.left = 0;
+	}
+	function menuHide (){
+		document.querySelector('#menu').style.left = '-230px';
+	}
+}
